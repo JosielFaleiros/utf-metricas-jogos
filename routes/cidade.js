182ec2729cc444c354ddf79627011941cf739a70
Rafael Santos <rafaelnsantos1@gmail.com>
2017-10-27 16:15:49 -0200

    pagina nova cidade

--- routes/cidade.js
+++ routes/cidade.js
@@ -4,6 +4,7 @@ var router = express.Router()
 var models = require('../model')
 
 router.get('/', async function(req, res) {
+    console.log(req.query)
     cidades = await models.cidade.getAll()
     res.render('cidades', {
         cidades: cidades
@@ -13,6 +14,10 @@ router.get('/', async function(req, res) {
 router.post('/new', async function(req, res) {
     novaCidade = await models.cidade.new(req.body)
     res.redirect('/cidade')
+})
+
+router.get('/new', function(req, res) {
+    res.render('novaCidade')
 })
 
 module.exports = router
